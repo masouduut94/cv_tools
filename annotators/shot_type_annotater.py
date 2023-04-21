8f4e8e6607694fe5c0544d20535085f37a06af50
masoud <masouduut94@gmail.com>
2023-04-21 05:16:34 -0700

    restructured the code.

--- annotators/shot_type_annotater.py
+++ annotators/shot_type_annotater.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from os.path import join
 import numpy as np
 import pandas as pd
+from shot_type_utils import *
 
 """
 Data must be like this:
@@ -15,100 +16,6 @@ Data must be like this:
 
 frame | x | y | back-hand/fore-hand/serve | push/lob/loop/flick
 """
-SKIP1 = 1
-SKIP2 = 10
-SKIP_WHEEL = 15
-SKIP3 = 200
-INTEGER_DEFAULT = -1
-
-
-def check_fno(fno, total_frame):
-    """
-    check if suggested frame number is not invalid based on video number of frames.
-    Args:
-        fno:
-        total_frame:
-
-    Returns:
-
-    """
-    if fno < 0:
-        print('\nInvaild !!! Jump to first image...')
-        return False
-    elif fno > total_frame:
-        print(f"\n maximum frames = {total_frame}")
-    else:
-        print(f"Frame set to: {fno}")
-        return True
-
-
-def to_frame(cap, df, current_fno, total_frame, save=False, custom_msg=None):
-    global current
-    print('current frame: ', current_fno)
-    if check_fno(current_fno, total_frame):
-        current = current_fno
-    cap.set(cv2.CAP_PROP_POS_FRAMES, current)
-    ret, frame = cap.read()
-    if save:
-        save_data(df, save_path)
-        print("The data is saved")
-    if not (current >= total_frame):
-        message = init_message(df, current, msg_cols, custom_msg)
-        if len(message):
-            print("MESSAGE:   ", " | ".join(message))
-    else:
-        df = save_data(df, save_path)
-        print("frame index bigger than number of frames.")
-    if not ret:
-        return None
-    else:
-        cv2.putText(frame, f'Frame: {current}/{total_frame}', (20, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
-        if len(message):
-            colors = [(0, 255, 0), (255, 255, 0), (255, 0, 0), (0, 0, 255)]
-            for i, (item, color) in enumerate(zip(message, colors)):
-                cv2.putText(frame, item, (100, 300+(i*50)), cv2.FONT_HERSHEY_PLAIN, 1.5, color, 2)
-        item = df.iloc[current]
-        if item.x != -1:
-            color = (0, 0, 255)
-            x = item.x
-            y = item.y
-            cv2.circle(frame, (x, y), 5, color, thickness=-1)
-        return frame
-
-
-def go_to_next(df: pd.DataFrame, column: str, value: Tuple[int, int], current: int, return_last=False):
-    next_indexes = df.index[df.frame > current]
-    msg = f"no more `{column}` after current value"
-    if not len(next_indexes):
-        print(msg)
-        return None, msg
-
-    temp = df[((df[column] == value[0]) | (df[column] == value[1])) & (df.index.isin(next_indexes))]
-    if len(temp):
-        if return_last:
-            return temp.iloc[-1].frame, ""
-        return temp.iloc[0].frame, ""
-    else:
-        print(msg)
-        return None, msg
-
-
-def go_to_previous(df: pd.DataFrame, column: str, value: Tuple[int, int], current: int, return_first=False):
-    next_indexes = df.index[df.frame < current]
-    msg = f"no more `{column}` before current value"
-    if not len(next_indexes):
-        print(msg)
-        return None, msg
-
-    temp = df[((df[column] == value[0]) | (df[column] == value[1])) & (df.index.isin(next_indexes))]
-    if len(temp):
-        if return_first:
-            return temp.iloc[0].frame, ""
-        return temp.iloc[-1].frame, ""
-    else:
-        print(msg)
-        return None, msg
-
 
 def click_and_crop(event, x, y, flags, param):
     # grab references to the global variables
@@ -135,97 +42,7 @@ def click_and_crop(event, x, y, flags, param):
         elif flags == -7864304:  # SHIFT + MOUSEWHEEL DOWN
             current -= SKIP3
         # current = (current + SKIP_WHEEL) if flags > 0 else (current - SKIP_WHEEL)
-        frame = to_frame(cap, df, current, n_frames)
-
-
-def init_message(df, index, columns, custom_msg=None):
-    # data = df.iloc[index]
-    items = []
-    st = ''
-
-    for col in columns:
-        if str(df.dtypes[col]).startswith('int'):
-            val = df.at[index, col]
-            if val == -1:
-                continue
-            reversed_dict = {val: key for (key, val) in all_dicts[col].items()}
-            items.append(f"{col}: {reversed_dict[val]}")
-            # st += f"{col}: {reversed_dict[val]} | "
-
-        elif str(df.dtypes[col]).startswith('bool'):
-            val = df.at[index, col]
-            if val:
-                items.append(f'{col} flag')
-                # st += f'{col} flag |'
-
-    if custom_msg:
-        items.insert(0, custom_msg)
-
-    # st = custom_msg if custom_msg is not None else st
-    # print(st)
-
-    return items
-
-
-def init(df: pd.DataFrame, cols_dtype: dict, with_fake_values: bool = False):
-    """
-    cols_dtype = {
-        'int': ['right_player', 'left_player', 'backhand', 'forehand']
-    }
-
-    :param df:
-    :param cols_dtype:
-    :param with_fake_values:
-    :return:
-    """
-    frames = np.arange(0, n_frames)
-    fake_positions = [-1] * n_frames
-    fake_positions2 = [0] * n_frames
-    fake_bool = [False] * n_frames
-    if with_fake_values:
-        data = {
-            'frame': frames,
-            'x': fake_positions,
-            'y': fake_positions
-        }
-        df = pd.DataFrame(data=data)
-
-    for col in ['x', 'y', 'frame']:
-        df[col] = df[col].astype('int32')
-
-    for dtype, columns in cols_dtype.items():
-        if dtype == 'int':
-            for col in columns:
-                if with_fake_values:
-                    df[col] = fake_positions
-                df[col] = df[col].astype('int')
-        elif dtype == 'bool':
-            for col in columns:
-                if with_fake_values:
-                    df[col] = fake_bool
-                df[col] = df[col].astype(bool)
-    return df
-
-
-def save_data(df, save_path):
-    df = df.sort_values(by=['frame'])
-    df.to_csv(save_path, index=False)
-    print(f"data saved automatically in {save_path}")
-    return df
-
-
-def toggle(df: pd.DataFrame, current: int, col: str, col_dict: dict):
-    current_val = df.at[current, col]
-    values = list(col_dict.values())
-    keys = list(col_dict.keys())
-    reversed_dict = {k: v for k, v in zip(values, keys)}
-    next_val = current_val + 1
-    try:
-        reversed_dict[next_val]
-    except KeyError:
-        next_val = INTEGER_DEFAULT
-    df.at[current, col] = next_val
-    return reversed_dict[next_val]
+        frame = to_frame(cap, df, current, save_path=save_path, total_frame=n_frames)
 
 
 if __name__ == '__main__':
@@ -280,17 +97,17 @@ if __name__ == '__main__':
 
     try:
         df = pd.read_csv(save_path)
-        df = init(df, cols_dtype)
+        df = init(df, cols_dtype, n_frames=n_frames)
         column = 'player'
         next_frame, msg = go_to_next(df, column=column, value=(0, 1), current=current, return_last=True)
         if next_frame is not None:
             frame = to_frame(cap, df, next_frame, n_frames, custom_msg=f'next {column}')
         print(f"loading from csv file {save_path}")
     except:
-        df = init(None, cols_dtype, with_fake_values=True)
+        df = init(None, cols_dtype, n_frames=n_frames, with_fake_values=True)
         print(f"failed to load {save_path}. initializing ......")
 
-    frame = to_frame(cap, df, current, n_frames)
+    frame = to_frame(cap, df, current, n_frames, save_path=save_path)
     cv2.namedWindow("image", cv2.WINDOW_AUTOSIZE)
     cv2.setMouseCallback("image", click_and_crop)
 
@@ -304,7 +121,7 @@ if __name__ == '__main__':
         if key == 27:  # Esc
             df = save_data(df, save_path)
             custom_msg = "Data is saved ..."
-            frame = to_frame(cap, df, current, n_frames, custom_msg=custom_msg)
+            frame = to_frame(cap, df, current, n_frames, custom_msg=custom_msg, save_path=save_path)
             break
 
         elif key == ord('1'):
@@ -313,77 +130,77 @@ if __name__ == '__main__':
             col = 'player'
             output = ''
             _ = toggle(df, current, col, all_dicts[col])
-            frame = to_frame(cap, df, current, n_frames, save=True)
+            frame = to_frame(cap, df, current, n_frames, save=True, save_path=save_path)
 
         elif key == ord('2'):
             # Toggle between values 0, 1, 2
             col = 'hand_type'
             _ = toggle(df, current, col, all_dicts[col])
-            frame = to_frame(cap, df, current, n_frames, save=True)
+            frame = to_frame(cap, df, current, n_frames, save=True, save_path=save_path)
 
         elif key == ord('3'):
             # Toggle between values 0, 1, 2
             col = 'shot_type'
             output = 'shot: '
             _ = toggle(df, current, col, all_dicts[col])
-            frame = to_frame(cap, df, current, n_frames, save=True)
+            frame = to_frame(cap, df, current, n_frames, save=True, save_path=save_path)
 
         elif key == ord('4'):
             # CHANGE (toss_end) FOR NEW WORK
             col = 'exclude'
             df.at[current, col] = False if df.at[current, col] else True
-            frame = to_frame(cap, df, current, n_frames, save=True)
+            frame = to_frame(cap, df, current, n_frames, save=True, save_path=save_path)
             print(current, f" {col}: {df.at[current, col]}")
 
         elif key == ord('5'):
             # CHANGE (toss_end) FOR NEW WORK
             col = 'exclude_end'
             df.at[current, col] = False if df.at[current, col] else True
-            frame = to_frame(cap, df, current, n_frames, save=True)
+            frame = to_frame(cap, df, current, n_frames, save=True, save_path=save_path)
             print(current, f" {col}: {df.at[current, col]}")
 
         elif key == ord('s'):
             df = save_data(df, save_path)
             custom_msg = "Data is saved ..."
-            frame = to_frame(cap, df, current, n_frames, custom_msg=custom_msg)
+            frame = to_frame(cap, df, current, n_frames, save_path=save_path, custom_msg=custom_msg)
         elif key == ord("f"):
             try:
                 check = int(input('Enter your frame:'))
             except:
                 print("not a valid number.")
                 check = current
-            frame = to_frame(cap, df, check, n_frames)
+            frame = to_frame(cap, df, check, n_frames, save_path=save_path)
         elif key == ord('x'):
             # Skip to next frame
             check = current + 1
-            frame = to_frame(cap, df, check, n_frames)
+            frame = to_frame(cap, df, check, n_frames, save_path=save_path)
         elif key == ord('z'):
             # Skip to previous frame
             check = current - 1
-            frame = to_frame(cap, df, check, n_frames)
+            frame = to_frame(cap, df, check, n_frames, save_path=save_path)
 
         elif key == 2555904:
             # Go to next unlabeled value (Right Arrow =>)
             column = 'player'
             next_frame, msg = go_to_next(df, column=column, value=(0, 1), current=current)
             if next_frame is not None:
-                frame = to_frame(cap, df, next_frame, n_frames, custom_msg=f'jumping next {column}')
+                frame = to_frame(cap, df, next_frame, n_frames, save_path=save_path,  custom_msg=f'jumping next {column}')
         elif key == 2424832:
             column = 'player'
             # Go to next unlabeled value (Left Arrow <=)
             next_frame, msg = go_to_previous(df, column=column, value=(0, 1), current=current)
             if next_frame is not None:
-                frame = to_frame(cap, df, next_frame, n_frames, custom_msg=f'jumping previous {column}')
+                frame = to_frame(cap, df, next_frame, n_frames, save_path=save_path,  custom_msg=f'jumping previous {column}')
         elif key == 2490368:
             # Go to next unlabeled value (Up Arrow <=)
             column = 'shot_type'
             next_frame, msg = go_to_next(df, column=column, value=tuple(range(0, 6)), current=current)
             if next_frame is not None:
-                frame = to_frame(cap, df, next_frame, n_frames, custom_msg=f'jumping next {column}')
+                frame = to_frame(cap, df, next_frame, n_frames, save_path=save_path,  custom_msg=f'jumping next {column}')
         elif key == 2621440:
             # Go to previous unlabeled value (Bottom Arrow)
             column = 'shot_type'
             next_frame, msg = go_to_previous(df, column=column, value=tuple(range(0, 5)), current=current)
             if next_frame is not None:
-                frame = to_frame(cap, df, next_frame, n_frames, custom_msg=f'jumping previous {column}')
+                frame = to_frame(cap, df, next_frame, n_frames, save_path=save_path, custom_msg=f'jumping previous {column}')
 
